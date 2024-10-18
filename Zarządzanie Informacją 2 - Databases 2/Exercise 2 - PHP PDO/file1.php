8db7518a6070c8a5f66cef277f03bc8531de2714
Igor Bębenek <139233781+igorbebenek@users.noreply.github.com>
2024-10-18 20:49:01 +0200

    PHP PDO

---
+++ Zarządzanie Informacją 2 - Databases 2/Exercise 2 - PHP PDO/file1.php
@@ -0,0 +1,29 @@
+﻿<?php
+//phpinfo();
+
+try {
+    $conn = new PDO('mysql:host=localhost;dbname=lab2', 'root', '');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $conn->exec("CREATE TABLE IF NOT EXISTS Animals (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,animal_name VARCHAR(100) NOT NULL, age INT NOT NULL)");
+    $conn->exec("DELETE FROM Animals");
+    $animals = array('Pies', 'Kot', 'Papuga', 'Królik', 'Żółw',NULL, 'Mysz', 'Koń', 'Słoń', 'Lew', 'Paw');
+    $conn->beginTransaction();
+    $insertStatement = $conn->prepare("INSERT INTO Animals (animal_name, age) VALUES (?, ?)");
+
+    foreach ($animals as $animal) {
+        $insertStatement->execute(array($animal, rand()));
+    }
+
+    $conn->commit();
+    $selectStatement = $conn->query("SELECT * FROM Animals");
+    while ($row = $selectStatement->fetch()) {
+        echo $row['animal_name'] . ' - ' . $row['age'] . "\n";
+    }
+
+} catch (PDOException $e) {
+    print $e->getMessage();
+    die();
+}
+
+?>
